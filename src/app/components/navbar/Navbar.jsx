ded45f8375d9664aaaf8a27797464b80e098e1c1
Nick Lamb <nlamb@hotmail.co.uk>
2021-11-03 11:32:01 +0000

    update logo on navbar

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './Navbar.scss';
 
 function Navbar() {
-  const appName = 'NeuraLink';
   return (
     <div className="navbar">
       <div className="navbar__left-section">
         <div className="navbar__logo-wrapper">
-          <h2 className="logo">{appName}</h2>
+          <h2 className="logo">Neura<span>List</span></h2>
         </div>
         <div className="search-bar">
           <form action="">
